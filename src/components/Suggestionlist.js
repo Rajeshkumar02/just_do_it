71e2209a8f1dd05b56769b5455f561543d8ef52a
Rajeshkumar S <69568501+Rajeshkumar02@users.noreply.github.com>
2021-08-23 20:05:20 +0530

    Create Suggestionlist.js

---
+++ src/components/Suggestionlist.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import "./css/Sugg.css";
+
+function Suggestionlist() {
+
+    return (
+        <div className="body1">
+            <div className="profile-container">
+                <div className="profile-card">
+                    <img src="https://i.imgur.com/bZBG9PE.jpg" alt="image1" className="profile-icon" />
+                    <div className="profile-name">Kelly Seikma</div>
+                    <div className="profile-position">Web Designer</div>
+                    <a href="#" className="button">Follow</a>
+                </div>
+            </div>
+        </div>
+
+    );
+}
+
+export default Suggestionlist;
