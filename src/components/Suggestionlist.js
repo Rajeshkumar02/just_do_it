5b249c98fcc1321377b41e5835340a5636b0e5ec
whitepirte <whitepirte@gmail.com>
2021-08-25 08:52:18 +0530

    home update with suggestions

--- src/components/Suggestionlist.js
+++ src/components/Suggestionlist.js
@@ -1,22 +1,33 @@
 import React from 'react';
 import "./css/Sugg.css";
-import { useState, useEffect } from "react";
+import { useState, useEffect,useContext } from "react";
 import { firebaseApp } from "../Firebase";
+import { AuthContext } from "./Auth";
 
 
 function Suggestionlist() {
 
     const [info, setInfo] = useState([]);
+    const { currentUser } = useContext(AuthContext);
+    const [error, setError] = useState([]);
 
     useEffect(() => {
+        var username=""
+        var firestore = firebaseApp.firestore().collection(currentUser.uid).doc("profile");
+        firestore.get().then((doc) => {
+            username = doc.data().Username;
+        }).catch((err) => {
+            setError(err.message);
+        })
         firebaseApp.firestore().collection("suggestions").get().then((querySnapshot) => {
             querySnapshot.forEach(element => {
                 var data = element.data();
+                if(username !== data.Username){
                 setInfo(arr => [
                     ...arr,
                     data
                 ]);
-
+            }
             });
         })
     }, []);
@@ -26,6 +37,7 @@ function Suggestionlist() {
             <div className="profile-container">
                 {info.map((data) => (<Frame imgurl={data.AvatarURL} name={data.Username} displayname={data.Displayname} aoi={data.AreaofInterest} />))}
             </div>
+            <h5 className="error">{error}</h5>
         </div>
     );
 
@@ -37,6 +49,7 @@ const Frame = ({ imgurl, displayname, name, aoi }) => {
             <div className="profile-name">{displayname}</div>
             <div className="display-name">@{name}</div>
             <div className="profile-position">{aoi}</div>
+            
             <div className="button">View Profile</div>
         </div>
     );
